08ffa3de6e32ac6cbb22ab0b8e1b9442dbed423a
island205 <island205@gmail.com>
2013-08-06 15:39:06 +0800

    build & deploy

--- Tea.js/module/js/greet/ruby.js
+++ Tea.js/module/js/greet/ruby.js
@@ -1,6 +1,6 @@
 Bodule.define('/js/greet/ruby',[], function (require, exports, module) {
     module.exports = {
-        helloLua:function () {
+        helloRuby:function () {
             console.log("Hello,ruby")
         } 
     }
